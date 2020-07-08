533434d89f36922ff5e7fa082be29acf6fa9b8d7
dasare <dasare10@outlook.com>
2020-07-08 15:26:17 -0400

    Determining variables
    Declaring variables required for project

---
+++ DatabaseMenu.java
@@ -0,0 +1,11 @@
+public class DatabaseMenu {
+    public static void main(String[] args) {
+
+        //Ask how many users to add
+        
+        //create x number of new users
+      
+    }
+
+
+}
---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student{   
+
+    private String firstName;
+    private String lastName;
+
+    private String classification;
+    private int studentID;
+    private String courses;
+    private int tuitBalance;
+    private int courseAmt = 600;
+    //prompt user to enter name and classifcation year
+
+    //generate user id
+
+    //enroll in courses
+
+    //view balance
+
+    //pay tuition
+
+    //show status
+
+}
