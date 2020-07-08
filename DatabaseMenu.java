db9b67cce21bfcfb2cef752e6d001eb1376f9147
dasare <dasare10@outlook.com>
2020-07-08 15:52:30 -0400

    Added user input
    Updated to allow for user input and provide sample output of program

--- DatabaseMenu.java
+++ DatabaseMenu.java
@@ -1,6 +1,8 @@
+//import java.util.*;
 public class DatabaseMenu {
     public static void main(String[] args) {
 
+        Student student1 = new Student();
         //Ask how many users to add
         
         //create x number of new users
--- Student.java
+++ Student.java
@@ -1,15 +1,27 @@
+import java.util.*;
 public class Student{   
 
     private String firstName;
     private String lastName;
 
-    private String classification;
+    private int classification;
     private int studentID;
     private String courses;
     private int tuitBalance;
     private int courseAmt = 600;
     //prompt user to enter name and classifcation year
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student's first name.");
+        this.firstName = in.nextLine();
 
+        System.out.println("Enter student's last name");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.classification = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + classification);
+    }
     //generate user id
 
     //enroll in courses
