1ce5fdc0470ff78bc06fa07fc04ed7e36206eb7d
dasare <dasare10@outlook.com>
2020-07-09 12:47:40 -0400

    Course Enrollment Method
    Added method to promt user to enter courses to enroll in. Used a do while loop to repeatedly ask user for input until user hits "Q" to quit.

--- DatabaseMenu.java
+++ DatabaseMenu.java
@@ -1,10 +1,10 @@
 //import java.util.*;
 public class DatabaseMenu {
-    public static void main(String[] args) {
+    public static void main( String[] args) {
 
-        Student student1 = new Student();
+         Student student1 = new Student();
         //Ask how many users to add
-        
+        student1.enroll();
         //create x number of new users
       
     }
--- Student.java
+++ Student.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+//import sun.tools.jstat.SymbolResolutionClosure;
+
 public class Student {
 
     private String firstName;
@@ -8,8 +10,8 @@ public class Student {
     private int classification;
     private static int id = 2000;
     private static int courseAmt = 600;
-    private String courses;
-    private int tuitBalance;
+    private String courses = null;
+    private int tuitBalance = 0;
 
     // prompt user to enter name and classifcation year
     public Student() {
@@ -26,17 +28,35 @@ public class Student {
         setStudentID();
 
         System.out.println(firstName + " " + lastName + " " + classification + " " + studentID);
-        
-        
+
     }
+
     // generate user id
-    private void setStudentID(){
-        //grade level + ID
+    private void setStudentID() {
+        // grade level + ID
         id++;
         this.studentID = classification + "" + id;
     }
 
     // enroll in courses
+    public void enroll() {
+        //Enter loop. User hits zero to exit.
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter course to enroll(Q to quit)");
+            String course = in.nextLine();
+
+            if (course != "Q") {
+                courses = courses + "\n" + course;
+                tuitBalance = tuitBalance + courseAmt;
+            } else {
+                break;
+            }
+        } while (1 != 0);
+
+        System.out.println("Enrolled in: " + courses);
+        System.out.println("Tuition Amt: " + tuitBalance);
+    }
 
     // view balance
 
